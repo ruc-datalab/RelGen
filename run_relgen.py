4398b7898270a8301c46398a27dfacce52c0e140
ltyzzz <ltyzzz@ruc.edu.cn>
2024-07-09 09:47:20 +0800

    feat: add comments in run_relgen.py

--- run_relgen.py
+++ run_relgen.py
@@ -21,17 +21,21 @@ args = parser.parse_args()
 data_dir = args.dir
 if not data_dir.endswith('/'):
     data_dir += '/'
-# 1. 加载元数据
+
+# Step1: Load metadata
 metadata = Metadata()
 metadata.load_from_json(data_dir + "metadata.json")
-# 2. 加载数据
+
+# Step2: Load real data
 data = {}
 for table in metadata.tables.keys():
     data[table] = pd.read_csv(data_dir + f"{table}.csv")
-# 3. 数据转换器
+
+# Step3: Initialize dataset transformer
 dataset = Dataset(metadata)
 dataset.fit(data)
-# 4. 数据合成器
+
+# Step4: Build synthesizer
 synthesis_method_map = {"single_model": SynthesisMethod.SINGLE_MODEL, "multi_model": SynthesisMethod.MULTI_MODEL}
 synthesizer_map = {
     "made": MADESynthesizer(dataset, method=synthesis_method_map[args.method]),
@@ -39,7 +43,8 @@ synthesizer_map = {
 }
 synthesizer = synthesizer_map[args.synthesizer]
 synthesizer.fit(data, epochs=10)
-# 5. 合成数据
+
+# Step5: Generate synthetic results
 sampled_data = synthesizer.sample()
 for table_name, table_data in sampled_data.items():
     table_data.to_csv(data_dir + f"synthetic_{table_name}.csv", index=None)
@@ -48,7 +53,8 @@ join_data = dataset.join_data(data)
 join_sampled_data = dataset.join_data(sampled_data)
 if len(metadata.tables) > 1:
     join_sampled_data.to_csv(data_dir + f"synthetic_all.csv", index=None)
-# 6. 数据评估器
+
+# Step6: Evaluate the synthetic results
 evaluator = Evaluator(join_data, join_sampled_data)
 evaluator.eval_fidelity(save_path=data_dir + "fidelity.csv")
 evaluator.eval_privacy(save_path=data_dir + "privacy.csv")
